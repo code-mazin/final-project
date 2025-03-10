36c66e999ae33625af826dce4021fb710032636b
Mazin <code.mazin.m@gmail.com>
2025-03-11 00:05:11 +1100

    Profile Page showing users jobs

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -3,12 +3,27 @@ import React from "react";
 // import styled from "styled-components";
 // import { Box } from "../styles";
 
-function Profile() {
+function Profile({user}) {
+    
     return (
         <div>
-            <h1>Big Dog</h1>
+            <h1>Username: {user.username}</h1>
+            <h2>Bio: {user.bio}</h2>
+            <h2>Email: {user.email}</h2>
+            <h2>age: {user.age}</h2>
+            <h2>years of experience: {user.years_of_exp}</h2>
+            <h2>Jobs: </h2>
+            <ul>
+                {user.jobs.map((job) => (
+                    <li key={user.id}>
+                        {job.title}
+                    </li>
+                ))}
+            </ul>
         </div>
-    )
-}
+    );
+    }
+    
+
 
 export default Profile;
